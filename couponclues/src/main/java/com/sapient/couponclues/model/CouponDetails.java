d708be712abc5483a351a43a7a78fdabdc49a117
lavnishl <lavnishl@yahoo.com>
2015-02-28 01:29:26 +0530

    testing123....

--- couponclues/src/main/java/com/sapient/couponclues/model/CouponDetails.java
+++ couponclues/src/main/java/com/sapient/couponclues/model/CouponDetails.java
@@ -1,29 +1,43 @@
 package com.sapient.couponclues.model;
 
-import java.util.Date;
-
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.elasticsearch.annotations.Document;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Document(indexName = "coupondetails", type = "coupondetails", refreshInterval = "-1")
 public class CouponDetails {
-    @Id
-    private String userId;
-    private String couponId;
-    private String couponIssuerSite;
-    private String discountPc;
-    private String issueDate;
-    private String validTill;
-    private String couponInfo;
-    private String prodId;
-    private Date updates;
-    private String updatedBy;
-    private String expireReason;
-    private String couponType;
+	
+	private String userId;
+	
+	@Id
+	private String couponId;
+
+	private String couponIssuerSite;
+
+	private String discountPc;
+
+	private String issueDate;
+
+	private String validTill;
+
+	private String couponInfo;
+
+	private String prodId;
+	
+	private String expireReason;
+
+	private String couponType;
+	
+	private String productCategory;
+	
+	private String productName;
+	
+	private String comments;
 
 }
