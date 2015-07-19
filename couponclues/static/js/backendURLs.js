ef3e9ce26eca6506d65e36c9a3fbc55b8c6d8570
grawat <gauravbrills14@gmail.com>
2015-07-19 14:15:30 +0530

    removing old static files

--- couponclues/static/js/backendURLs.js
+++
@@ -1,8 +0,0 @@
-(function(){
-	'use strict';
-	couponFetcher.services.constant('backendURLs',{
-        output : 'http://localhost:8080/brain/fetchCouponsILove',
-        productMaster : 'data/ProductMaster.json'
-	})
-
-})();
